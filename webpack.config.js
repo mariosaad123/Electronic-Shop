61fbca385dad72d8ce6fc3720fe7ab1fbd432571
mariosaad123 <120504233+mariosaad123@users.noreply.github.com>
2024-10-19 21:34:12 +0300

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,107 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: './src/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    hot: false,
+    port: 9000,
+    open: true,
+    devMiddleware: {
+      writeToDisk: true,
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+        options: {
+            minimize: true,
+          },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/i,
+        exclude: /bootstrap\.min\.css$/i,
+        use: [ 
+        {
+          loader: MiniCssExtractPlugin.loader,
+          options: {
+            esModule: false,
+          },
+        } 
+         ,
+           "css-loader",
+          "sass-loader"],
+      },
+      {
+        test: /bootstrap\.min\.css$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              esModule: false,
+            },
+          },
+          'rtlcss-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: "./images/[name][ext]"
+        }
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: "./fonts/[name][ext]"
+        }
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        filename: "index.html",
+        template: "./src/index.html"
+    }),
+    new HtmlWebpackPlugin({
+      filename: "product.html",
+      template: "./src/product.html"
+  }),
+  new HtmlWebpackPlugin({
+    filename: "checkout.html",
+    template: "./src/checkout.html"
+}),
+  new HtmlWebpackPlugin({
+    filename: "payment.html",
+    template: "./src/payment.html"
+  }),
+  new HtmlWebpackPlugin({
+    filename: "search.html",
+    template: "./src/search.html"
+}),
+new HtmlWebpackPlugin({
+  filename: "contact.html",
+  template: "./src/contact.html"
+}),
+    new MiniCssExtractPlugin({
+      filename: "css/style.css"
+    }),
+    new CssMinimizerPlugin()
+  ]
+};
